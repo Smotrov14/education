7f2cd529465648e36bcb665085cbf7364019ba71
Smotrov14 <smotrov144@gmail.com>
2025-01-30 21:21:02 +0300

    Go

--- src/Computer.java
+++ src/Computer.java
@@ -30,14 +30,9 @@ public class Computer {
         return vendor;
     }
 
-
-      //  Hdd hdd =  new Hdd(900, 1024);
-       // Display display = new Display(3200,27);
-       // Keyboard keyboard = new Keyboard(700);
  public int getAllWeight(){
      return (cpu.getWeight()+ memory.getWeight()+ hdd.getWeight()+ display.getWeight()+ keyboard.getWeight());
  }
-
 
 public String getAllInfo(){
      return "компьтер бренда " + vendor + " модель компьютера " + name + cpu.toString() + memory.toString() + hdd.toString() + display.toString() + keyboard.toString();
